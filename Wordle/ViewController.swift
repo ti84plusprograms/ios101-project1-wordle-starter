69c350eca26d2e14714644cc44fe4c9628bb848a
Agarwal, Aaryaman <aagarwal485@gatech.edu>
2025-06-08 21:12:39 -0400

    completed project 1

--- Wordle/ViewController.swift
+++ Wordle/ViewController.swift
@@ -30,7 +30,13 @@ class ViewController: UIViewController {
       Else, it should use the `enter` method of `boardController` and pass in the selected string as the argument.
      */
     // START YOUR CODE HERE
-    // ...
+    keyboardController.didSelectString = { [weak self] string in
+        if string == DELETE_KEY {
+              self?.boardController.deleteLastCharacter()
+          } else {
+              self?.boardController.enter(string)
+          }
+      }
     // END YOUR CODE HERE
   }
 }
